e6e2706d8bc4f62ca20f14d2c1953e487b70ffb2
l.cointrel <l.cointrel@mediactive.fr>
2020-03-26 23:33:40 +0100

    Auto adding commands

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-const { getPath } = require('../libs/helpers');
-
 const fs = require('fs');
 
-console.log('getPath', getPath('package.json'))
+// console.log('getPath', getPath('package.json'))
 
-console.log('process.mainModule.path', process.mainModule.path)
+// console.log('process.mainModule.path', process.mainModule.path)
 
-console.log('process.env.INIT_CWD', process.env.INIT_CWD)
+// console.log('process.env.INIT_CWD', process.env.INIT_CWD)
+
+let thePathPackageJson = path.join(process.env.INIT_CWD, 'package.json');
 
 
 let scriptsCommands = {
@@ -17,18 +17,20 @@ let scriptsCommands = {
     'packify:watch': 'cross-env NODE_ENV=development node_modules/@ludoows/packify/cli/init.js --config=node_modules/@ludoows/packify/configs/packify.js --watch'
 }
 
-let thePackageJson = fs.readFileSync(getPath('package.json'), 'utf-8');
+let thePackageJson = fs.readFileSync(thePathPackageJson, 'utf-8');
 
 let parsedJson = JSON.parse(thePackageJson);
 
 let keysScript = Object.keys(scriptsCommands);
 
 keysScript.forEach((key) => {
-    parsedJson.scripts[key] = scriptsCommands[key];
+    if(parsedJson.scripts[key] === undefined) {
+        parsedJson.scripts[key] = scriptsCommands[key];
+    }
 })
 
-fs.writeFileSync(getPath('package.json'), '');
-fs.writeFileSync(getPath('package.json'), JSON.stringify(parsedJson))
+fs.writeFileSync(thePathPackageJson, '');
+fs.writeFileSync(thePathPackageJson, JSON.stringify(parsedJson))
 
 
 
