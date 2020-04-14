879c1ac4aeca9c95d05e7987c31b5c1effc8f7eb
l.cointrel <l.cointrel@mediactive.fr>
2020-04-14 17:09:14 +0200

    Focus on extractor js

--- configs/packify.config.js
+++ configs/packify.config.js
@@ -7,8 +7,8 @@ const {
 } = require('../libs/helpers');
 
 const folders = [
-    getPath('resources'),
-    getPath('public', 'images')
+    getPath('resources', 'assets'),
+    // getPath('public', 'images')
 ]
 
 let assets = [];
