72e8da0c377edd34fc008d563284c163b88b2b43
l.cointrel <l.cointrel@mediactive.fr>
2020-04-29 00:12:27 +0200

    Clean code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
     packify: require('./libs/core'),
     helpers: require('./libs/helpers'),
-    plugin: require('./libs/plugin'),
-    parser: require('./libs/parser')
+    plugin: require('./libs/plugin')
 }
